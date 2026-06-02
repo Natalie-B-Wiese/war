b6be58fa55eebc3504fe6defa1c4520c115a0809
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-06-01 20:08:19 -0400

    Write tests for player

--- lib/war_player.rb
+++ lib/war_player.rb
@@ -10,20 +10,6 @@ class WarPlayer
     @cards.push(card)
   end
 
-  def take_top_card
-    @cards.shift
-  end
-
-  def top_card_value
-    top_card.value
-  end
-
-  def top_card
-    @cards[0]
-  end
-
-  def card_count
-    @cards.length
-  end
+  
 
 end 
--- spec/war_player_spec.rb
+++ spec/war_player_spec.rb
@@ -25,10 +25,90 @@ describe 'WarPlayer' do
   end
 
   describe '#take_top_card' do
-    it 'returns a card object' do
-      
+    it 'returns top card object' do
+      player=WarPlayer.new('Natalie')
+      card1=PlayingCard.new('A', 'Spades')
+      card2=PlayingCard.new('K', 'Hearts')
+      player.add_card(card1)
+      player.add_card(card2)
+
+      result=player.take_top_card
+      expect(result).to eq card1
+    end
+
+    it 'removes card from cards' do
+      player=WarPlayer.new('Natalie')
+      card1=PlayingCard.new('A', 'Spades')
+      card2=PlayingCard.new('K', 'Hearts')
+      player.add_card(card1)
+      player.add_card(card2)
+
+      player.take_top_card
+      result=player.card_count
+      expect(result).to eq 1
     end
     
+  end
+
+  describe '#top_card' do
+    it 'returns top card object' do
+      player=WarPlayer.new('Natalie')
+      card1=PlayingCard.new('A', 'Spades')
+      card2=PlayingCard.new('K', 'Hearts')
+      player.add_card(card1)
+      player.add_card(card2)
+
+      result=player.top_card
+      expect(result).to eq card1
+    end
+    
+  end
+
+  describe '#top_card_value' do
+    it 'returns value of top card object' do
+      player=WarPlayer.new('Natalie')
+      card1=PlayingCard.new('2', 'Spades')
+      card2=PlayingCard.new('K', 'Hearts')
+      player.add_card(card1)
+      player.add_card(card2)
+
+      result=player.top_card_value
+      expected_value=card1.value
+      expect(result).to eq expected_value
+    end
+    
+  end
+
+  describe '#card_count' do
+    context 'when the player has no cards' do
+      it 'returns 0' do
+        player=WarPlayer.new('Natalie')
+        result=player.card_count
+        expect(result).to eq 0
+      end
+    end
+
+    context 'when there is 1 card' do
+      it 'returns 1' do
+        player=WarPlayer.new('Natalie')
+        player.add_card(PlayingCard.new('A', 'Spades'))
+
+        result=player.card_count
+        expect(result).to eq 1
+      end
+    end
+
+    context 'when there are many cards' do
+      it 'returns correct number' do
+        player=WarPlayer.new('Natalie')
+        player.add_card(PlayingCard.new('A', 'Spades'))
+        player.add_card(PlayingCard.new('J', 'Diamonds'))
+        player.add_card(PlayingCard.new('3', 'Hearts'))
+
+        result=player.card_count
+        expect(result).to eq 3
+      end
+    end
   end
 
 end
